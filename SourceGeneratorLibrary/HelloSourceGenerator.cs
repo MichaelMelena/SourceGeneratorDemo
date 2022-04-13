d735859b1f3f6e74e52c0623f66ee2071f959982
Michael Melena <melena@havit.cz>
2022-04-13 16:06:42 +0200

    added other library wip

--- SourceGeneratorLibrary/HelloSourceGenerator.cs
+++ SourceGeneratorLibrary/HelloSourceGenerator.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.CodeAnalysis;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace SourceGeneratorLibrary
@@ -12,6 +15,15 @@ namespace SourceGeneratorLibrary
         {
             // Code generation goes here
 
+            //context.Compilation.SourceModule.ReferencedAssemblies.First(x => x.Name == "OtherLibrary").Name;
+
+            var compilation = context.Compilation.WithReferences(context.Compilation.References.First(x => x.Display == "OtherLibrary"));
+            string text = compilation.AssemblyName;
+
+            //var file = Path.GetFileNameWithoutExtension(context.Compilation.SyntaxTrees.First().FilePath);
+            //var rootDir = Path.GetDirectoryName("../OtherLibarry");
+            //var files = Directory.EnumerateFiles(rootDir, "*.resx", SearchOption.AllDirectories);
+            //string text = string.Join("\n", files);
             // Find the main method
             var mainMethod = context.Compilation.GetEntryPoint(context.CancellationToken);
             // Build up the source code
@@ -22,8 +34,12 @@ namespace {mainMethod.ContainingNamespace.ToDisplayString()}
 {{
     public static partial class {mainMethod.ContainingType.Name}
     {{
-        static partial void HelloFrom(string name) =>
+        static partial void HelloFrom(string name)
+        {{
+            Console.WriteLine(""{text}"");
             Console.WriteLine($""Generator says: Hi from '{{name}}'"");
+         
+        }}
     }}
 }}
 ";
@@ -37,7 +53,13 @@ namespace {mainMethod.ContainingNamespace.ToDisplayString()}
 
         public void Initialize(GeneratorInitializationContext context)
         {
-            // No initialization required for this one
+#if DEBUG
+            if (!Debugger.IsAttached)
+            {
+                Debugger.Launch();
+            }
+#endif 
+            Debug.WriteLine("Initalize code generator");
         }
     }
 }
